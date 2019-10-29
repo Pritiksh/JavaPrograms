33fdb955b8d8f48feeb3daff95bb3561b355eb04
Priti keshari <kesharipriti@gmail.com>
2019-10-29 11:13:38 +0530

    “programs”

---
+++ src/programs/BothArrayEualOrNot.java
@@ -0,0 +1,26 @@
+package programs;
+
+public class BothArrayEualOrNot {
+
+	public static void main(String[] args) {
+		int arr1[]={1,2,3,4};
+		int arr2[]={1,2,3};
+		boolean equalOrNot=true;
+		if(arr1.length==arr2.length){
+			for(int i=0;i<arr1.length;i++){
+				if(arr1[i]!=arr2[i]){
+					equalOrNot=false;
+				}
+			}
+		}
+		else{
+			equalOrNot=false;
+		}
+		if(equalOrNot)
+			System.out.println("Both array are equal");
+		else
+			System.out.println("Both arrays are not equal");
+	}
+
+}
+
--- src/programs/PrimeNumber.java
+++ src/programs/PrimeNumber.java
@@ -1,21 +1,25 @@
 package programs;
-
+/*
+ * Prime number programs
+ */
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		 int num = 4;
-	        boolean flag = false;
-	        for(int i = 2; i <= num/2; ++i)
-	        {
-	            if(num % i == 0)
-	            {
-	                flag = true;
-	                break;
-	            }
-	        }
-	        if (!flag)
-	            System.out.println(num + " is a prime number.");
-	        else
-	            System.out.println(num + " is not a prime number.");
-	    }
+		int num = 29;
+		boolean flag = false;
+		for(int i = 2; i <= num/2; ++i)
+		{
+
+			if(num % i == 0)
+			{
+				flag = true;
+				break;
+			}
+		}
+		if (!flag)
+			System.out.println(num + " is a prime number.");
+		else
+			System.out.println(num + " is not a prime number.");
 	}
+}
+
