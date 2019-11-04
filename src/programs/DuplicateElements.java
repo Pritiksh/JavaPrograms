cd7de8f5dd7d83db92f15e830e40202e4884e73e
Priti keshari <kesharipriti@gmail.com>
2019-11-04 14:38:24 +0530

    Write a java program to find duplicate elements from string array

---
+++ src/programs/DuplicateElements.java
@@ -0,0 +1,18 @@
+package programs;
+
+import java.util.HashSet;
+
+//Write a java program to find duplicate elements from array
+public class DuplicateElements {
+
+	public static void main(String[] args) {
+		String[] s = { "apple", "orange", "apple", "banana" };
+		HashSet<String> hs = new HashSet<String>();
+		for (String dup : s) {
+			if (!hs.add(dup)) {
+				System.out.println(dup);
+			}
+		}
+	}
+
+}
