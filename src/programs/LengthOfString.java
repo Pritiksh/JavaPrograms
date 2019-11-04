f9b6ceaf7e770696f8cde08d56866a777708554a
Priti keshari <kesharipriti@gmail.com>
2019-11-04 15:38:50 +0530

    program to find length of a string without using length()

---
+++ src/programs/LengthOfString.java
@@ -0,0 +1,16 @@
+package programs;
+//Write a program to find length of a string without using length().
+
+public class LengthOfString {
+
+	public static void main(String[] args) {
+		int count = 0;
+		String str = "Priti";
+		for(int i=0;i<str.length();i++){
+			count++;
+		}
+		System.out.println(count);
+		
+	}
+
+}
